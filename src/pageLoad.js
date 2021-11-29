dc6176bc6c0d88e900837e1ef02fdc13569efd7c
kleinezicke <julia.neuburger@24speed.at>
2021-11-29 20:54:04 +0100

    creating header using js

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,13 +1,39 @@
-import restaurant from "./restaurant.jpg";
-
 const pageLoad = () => {
     
     const content = document.querySelector("#content");
 
+    const header = document.createElement("header");
+    document.body.insertBefore(header, content)
+
+    const nav = document.createElement("nav")
+    header.appendChild(nav);
+    const tabs = document.createElement("ul")
+    nav.appendChild(tabs);
+
+    const footer = document.createElement("footer")
+    document.body.appendChild(footer);
+
+    const createTab = (name, id = name.toLowerCase()) => {
+        const tab = document.createElement("li")
+        tab.textContent = name;
+        tab.id = id;
+
+        tabs.appendChild(tab);
+    }
+
+    const restaurantName = document.createElement("h1")
+    restaurantName.textContent = "Questionable Food";
+    tabs.appendChild(restaurantName);
+
+    createTab("Home");
+    createTab("Menu");
+    createTab("Contact");
+    /*const content = document.querySelector("#content");
+
     const h1 = document.createElement("h1");
     h1.textContent = "Questionable Food";
 
-    const img = document.createElement("img")
+    const img = new Image();
     img.src = restaurant;
 
     const  text = document.createElement("p");
@@ -15,7 +41,7 @@ const pageLoad = () => {
 
     content.appendChild(h1);
     content.appendChild(img);
-    content.appendChild(text)
+    content.appendChild(text)*/
 }
 
 export default pageLoad;
