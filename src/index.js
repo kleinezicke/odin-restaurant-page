97f89f2fc2845e222b9efeb32e89f1eeb00e27a7
kleinezicke <julia.neuburger@24speed.at>
2021-11-29 22:41:11 +0100

    clear function added and event on click for home

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import loadHome from "./home.js";
 
 pageLoad();
 loadHome();
+
+const clear = () => {
+    const content = document.querySelector("#content");
+    const children = content.childNodes;
+
+    for (let i = 0, l = children.length; i < l; i++) {
+        content.removeChild(content.firstChild);
+    }
+}
+
+const events = () => {
+    const home = document.getElementById("home")
+    home.addEventListener("click", function(){
+        clear();
+        loadHome();
+    });
+}
+
+events();
